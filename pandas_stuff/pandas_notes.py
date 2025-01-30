22f47fa36fd875988736e1397d39181112b27107
Daniel Grimut <grimut.daniel@gmail.com>
2025-01-29 19:54:09 -0500

    pandas notes done

--- pandas_stuff/pandas_notes.py
+++ pandas_stuff/pandas_notes.py
@@ -6,8 +6,13 @@ import numpy as np
 # Serie's elements must be of the same type (homogenious) + all elements associated to a label
 data = pdiddy.Series([10, 23, 56, 17])
 
-# pandas sees everything as a 2D array (like a table), so even if you try to add more items, we 
-# stay in table format (2D) in the end
+# Another example
+ser = pdiddy.Series([100, 200, 300, 400], index=['apple', 'ball', 'cat', 'dog'])
+print(ser)
+print(ser * 2) # ELEMENET wise opperation
+
+# Series =  A one-dimensional labeled array
+# DF = A two-dimensional labeled data structure similar to a table
 # data = pdiddy.Series([[10, 23, 56, 17], [10, 23, 56, 17]])
 print(data)
 
@@ -22,7 +27,7 @@ print("--------------------")
 
 print(f'names here:\n{df["Name"]}') # Select column
 print(df.loc[0]) # Select row by index - the index is the LABEL, in this case Label=Index of the row
-print(df.iloc[0, 1]) # Select element by position
+print(f'iloc with arr: {df.iloc[0, 1]}') # Select element by position - row 0, col 1
 print(df[1:3]) # Slice rows
 print(df.loc[:, 'Name':'Age']) # Slice rows and columns
 
@@ -44,5 +49,12 @@ print(f'DF after fillna:\n {df}')
 # Essentialy saying "create a new dataframe from df, but only keep rows from df where age > 30"
 # under the hood df['Age'] > 30 is going to go over all the cells of the table and create a 
 # new boolean 1D array/Series (mask), which will indicate which rows to keep from the DF.
-df = df[df['Age'] > 30]
-print(f'DF after filter:\n {df}')
+df_bool = df[df['Age'] > 30]
+print(f'DF after filter:\n {df_bool}')
+
+# # summariszing data using groupby
+grouped = df.groupby('Age')
+print(grouped)
+
+# Concatenation: pd.concat([df1, df2]) = append df2 to the side/bellow of df1
+# Merging: pd.merge(df1, df2, on='key') = similar to the SQL JOIN - merges on some key
